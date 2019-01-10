ea38665c69382882165bc6999c1a233dbe854520
TimHunter <thetimmeh@hotmail.com>
2019-01-09 18:32:34 -0600

    New Modes
    Added new modes and reworked the card data system.

--- js/Deck.js
+++ js/Deck.js
@@ -1,14 +1,50 @@
 /* global $ */
 
-//Hold new instances of the deck.
-var cardDeck;
+//Hold cards that have not been drawn.
+var cardDeck = [];
 
-//Hold deck data pulled from JSON file.
-var cardData;
+//Hold cards that have already been drawn.
+var drawnDeck = [];
 
-//Randomly shuffles the cardDeck array.
-function shuffleDeck() {
-  var currentIndex = cardDeck.length, temporaryValue, randomIndex;
+//State check for whether the deck has been built.
+var deckBuilt = false;
+
+//Card object holds imported card data.
+function Card(name, description, type, terra, sol, luna, mercurius, image) {
+    this.name = name;
+    this.description = description;
+    this.type = type;
+    this.terra = terra;
+    this.sol = sol;
+    this.luna = luna;
+    this.mercurius = mercurius;
+    this.image = image;
+}
+
+//Grabs card data and stores it in cardDeck.
+function buildDeck() {
+    //Create defer for tracking when data is done being imported
+    var def = $.Deferred();
+    //Import data from JSON file
+    $.getJSON(
+        'cards/CardData.json', 
+        function(data) {
+            for(var i = 0; i < data.length; i++){
+                var newCard = new Card(data[i].name, data[i].description, data[i].type, data[i].terra, data[i].sol, data[i].luna, data[i].mercurius, data[i].image);
+                cardDeck.push(newCard);
+            }
+        }
+     ).done(
+        function(){
+            def.resolve();
+        }
+    );
+    return def;
+}
+
+//Randomly shuffles the given deck.
+function shuffleDeck(deck) {
+  var currentIndex = deck.length, temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
@@ -18,72 +54,110 @@ function shuffleDeck() {
     currentIndex -= 1;
 
     // And swap it with the current element.
-    temporaryValue = cardDeck[currentIndex];
-    cardDeck[currentIndex] = cardDeck[randomIndex];
-    cardDeck[randomIndex] = temporaryValue;
+    temporaryValue = deck[currentIndex];
+    deck[currentIndex] = deck[randomIndex];
+    deck[randomIndex] = temporaryValue;
   }
 }
 
-//Shifts card from cardDeck and sets it to website.
-function showCards() {
-    $("#cards").empty();
-    //cardDeck.shift().appendTo("#cards");
-    for(var i = 0; i < $('select[id="numOfCards"] option:selected').val(); i++) {
-        //Draw card
-        var card = cardDeck.shift();
-        
-        //Build container
-        var cardContainer = $("<div>");
-        cardContainer.attr({
-            'id': "card" + i
-        });
-        cardContainer.appendTo("#cards");
-        
-        //Build image
-        var img = $("<img>");
-        img.attr({
-           "id":  card.name,
-            "src": card.image,
-            "title": card.name + " Image"
-        });
-        img.appendTo(cardContainer);
-        
-        var desc = $("<p>");
-        desc.html(card.description);
-        desc.appendTo(cardContainer);
-    } 
+//Puts drawnDeck cards back into cardDeck.
+function combineDecks() {
+    while(drawnDeck.length != 0) {
+        cardDeck.push(drawnDeck.pop());
+    }
 }
 
-//Grabs card data and stores it. Makes new copies of card data for cardDeck.
-function buildDeck() {
-    var def = $.Deferred();
-    if(cardData == null){
-        $.getJSON(
-            'cards/CardData.json', 
-            function(data) {
-                cardData = data;
-                cardDeck = Array.from(data);
-            }
-         ).done(
-            function(){
-                def.resolve();
-            }
-        );
-    } else {
-        cardDeck = Array.from(cardData);
-        def.resolve();
+//Picks which card mode based on selection.
+function selectCards() {
+    var i;
+    
+    //Clear card display
+    $("#cards").empty();
+    
+    //Get select value
+    var selection = $('select[id="numOfCards"] option:selected').val();
+    
+    //Mode 1: Draw random cards from entire deck.
+    if(selection == 1 || selection == 3 || selection == 8) {
+        for(i = 0; i < selection; i++) {
+            var card = cardDeck.shift();
+            drawnDeck.push(card);
+            showCard(card);
+        } 
     }
-    return def;
+    //Mode 2: First 4 are Celestials, then random for the rest.
+    if(selection == 12) {
+        var celestials = cardDeck.filter(function(card) {return card.type == "Celestial";});
+        var nonCelestials = cardDeck.filter(function(card) {return card.type != "Celestial";});
+        
+        for(i = 0; i < celestials.length; i++) {
+            showCard(celestials[i]);
+        }
+        
+        shuffleDeck(nonCelestials);
+        for(i = 0; i < selection - celestials.length; i++) {
+            showCard(nonCelestials.shift());
+        }
+    }
+    //Mode 3: First 8 are Chakras and the Serpent, then random for the rest.
+    if(selection == 24) {
+        var findList = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet", "Serpent"];
+        
+        for(i = 0; i < findList.length; i++) {
+            var findCard = cardDeck.find(function(card) {return card.name == findList[i];});
+            showCard(findCard);
+        }
+        
+        var remaining = cardDeck.filter(function(card){ return !((card.type == "Chakra") || (card.type == "Serpent"));});
+        shuffleDeck(remaining);
+        for(i = 0; i < selection - findList.length; i++) {
+            showCard(remaining.shift());
+        }  
+    }
+}
+
+//Draws cards onto the website.
+function showCard(card) {
+    //Build container
+    var cardContainer = $("<div>");
+    cardContainer.attr({
+        'id': card.name + "Container"
+    });
+    cardContainer.appendTo("#cards");
+
+    //Build image
+    var img = $("<img>");
+    img.attr({
+       "id":  card.name + "Image",
+        "src": card.image,
+        "title": card.name + " Image"
+    });
+    img.appendTo(cardContainer);
+
+    //Build description
+    var desc = $("<p>");
+    desc.attr({
+       "id": card.name + "Description" 
+    });
+    desc.html(card.description);
+    desc.appendTo(cardContainer);
 }
 
 //Combines other functions into correct order.
 function drawCards() {
-    buildDeck().done(
-        function () {
-            shuffleDeck();
-            showCards();
-        }
-    );
+    if(!deckBuilt){
+        buildDeck().done(
+            function () {
+                deckBuilt = true;
+                shuffleDeck(cardDeck);
+                selectCards();
+            }
+        );
+    } else {
+        combineDecks();
+        shuffleDeck(cardDeck);
+        selectCards();
+    }
 }
 
 //Set functions when document is ready.
